2163164418d16473fbe4687b4e6f5fe91b1ea744
songgouen <zlxl6445@naver.com>
2024-01-30 23:31:19 +0900

    [feat] adding login page ui

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
+import router from '@/routes/Routes';
 import { RouterProvider } from 'react-router-dom';
 import store from './stores/store';
-import router from '@/routes/Routes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
